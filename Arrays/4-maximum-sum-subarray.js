d476076365df18cfeeb79958fb8a8a14c8f246d1
milindkusahu <milindsahu2000@gmail.com>
2024-11-19 22:45:04 +0530

    Kadane's Algorithm

---
+++ Arrays/4-maximum-sum-subarray.js
@@ -0,0 +1,63 @@
+/**
+ * Ques 4 - Given an integer array nums, find the subarray with the largest sum, and return its sum.
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4]     ----->>    Output: 6,   [4,-1,2,1]
+ * Input: [5,4,-1,7,8]                ----->>    Output: 23,  [5,4,-1,7,8]
+ */
+
+// Brute Force Approach
+
+function maxSubArray(nums) {
+  let maxSum = nums[0];
+  let size = nums.length;
+
+  for (let i = 0; i < size; i++) {
+    // O(n)
+    let currentSum = 0;
+    for (let j = i; j < size; j++) {
+      // O(n)
+      currentSum = currentSum + nums[j];
+      if (currentSum > maxSum) {
+        maxSum = currentSum;
+      }
+    }
+  }
+
+  return maxSum;
+}
+
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Output => 6
+// console.log(maxSubArray([5, 4, -1, 7, 8])); // Output => 23
+
+/**
+ * Time Complexity => O(n^2)
+ * Space Complexity => O(1)
+ */
+
+// Kadane's Algorithm
+
+function maxSubArrayOptimized(nums) {
+  let sum = 0;
+  let max = nums[0];
+
+  for (let i = 0; i < nums.length; i++) {
+    // O(n)
+    sum = sum + nums[i];
+
+    if (sum > max) {
+      max = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return max;
+}
+
+console.log(maxSubArrayOptimized([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Output => 6
+console.log(maxSubArrayOptimized([5, 4, -1, 7, 8])); // Output => 23
+
+/**
+ * Time Complexity => O(n)
+ * Space Complexity => O(1)
+ */
