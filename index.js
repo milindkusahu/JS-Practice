05810cb9150877ab377c69a0fa642968d4e98fda
milindkusahu <milindsahu2000@gmail.com>
2024-11-22 22:29:07 +0530

    sorting updated

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ let nums = [1, 2, 3, 3];
 let sort = nums.sort((a, b) => a - b);
 
 console.log(sort);
+
+
+let nums1 = [1, 2, 3, 3];
+
+let sort1 = nums.sort((a, b) => b - a);
+
+console.log(sort);
