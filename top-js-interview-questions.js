7a0b1f2656f879a64b89ddf75b9def6dfe21ee4e
milindkusahu <milindsahu2000@gmail.com>
2025-02-06 17:52:24 +0530

    JavaScript Interview Questions

---
+++ top-js-interview-questions.js
@@ -0,0 +1,32 @@
+/* Question: 1 */
+
+// let date = new Date().getFullYear();
+// console.log(date); // 2025
+
+/* Question: 2 */
+
+// const pi = Math.PI;
+// console.log(pi); // 3.141592653589793
+
+/* Question: 3 */
+
+// console.log(typeof null); // object
+// console.log(typeof NaN); // number
+
+/* Question: 4 */
+
+// let obj = {
+//   name: "Milind",
+//   email: "contact@milindsahu.com",
+//   age: "24",
+// };
+
+// for (let key in obj) {
+//   console.log(obj[key]);
+// }
+
+/* Question: 5 */
+
+// let arr = ["a", "b", "c", "d", "e"];
+// arr.forEach((ele, i) => console.log(ele, i));
+
